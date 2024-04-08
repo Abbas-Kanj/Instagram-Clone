ea9b4714f09a3d399119f5504252d95c28c88a4d
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-04-08 06:23:03 +0300

    added error handling on signup

--- client/src/pages/Login/components/SignUpForm.jsx
+++ client/src/pages/Login/components/SignUpForm.jsx
@@ -5,41 +5,52 @@ import "../index.css";
 const SignUpForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const validateForm = () => {
+    if (email == "" || password == "") {
+      setError("Please fill empty fields");
+      return false;
+    } else {
+      setError("");
+      return true;
+    }
+  };
 
   const handleSignup = async () => {
-    let data = new FormData();
-    data.append("email", email);
-    data.append("password", password);
-    try {
-      const headers = { "Content-Type": "multipart/form-data" };
-      const res = await sendRequest("POST", "/api/register", data, headers);
-      console.log(res.data);
-      if ((res.status = 200)) {
-        window.localStorage.setItem("token", res.data.authorisation.token);
-        console.log("sign up successfull");
+    if (validateForm()) {
+      let data = new FormData();
+      data.append("email", email);
+      data.append("password", password);
+      try {
+        const headers = { "Content-Type": "multipart/form-data" };
+        const res = await sendRequest("POST", "/api/register", data, headers);
+        console.log(res.data);
+        if ((res.status = 200)) {
+          window.localStorage.setItem("token", res.data.authorisation.token);
+          console.log("sign up successfull");
+        }
+      } catch (error) {
+        console.log(error);
       }
-    } catch (error) {
-      console.log(error);
     }
   };
 
   return (
     <div className="flex column align-center">
-      <div className="flex column align-center justify-evenly bg-white form ">
+      <div className="flex column align-center justify-around bg-white form ">
         <h1 className="text-black">Instagram</h1>
         <form className="flex column big-gap p form-container align-center">
           <input
             type="email"
             label="Email address"
             placeholder="Email"
-            size="lg"
             onChange={(e) => setEmail(e.target.value)}
           />
           <input
             type="password"
             label="Password"
             placeholder="Password"
-            size="lg"
             onChange={(e) => setPassword(e.target.value)}
           />
           <div className=" flex column align-center big-gap">
@@ -56,6 +67,13 @@ const SignUpForm = () => {
           >
             Sign up
           </button>
+          <div className="flex gap">
+            <p className="text-black">Already have an account?</p>
+            <a className=" text-blue cursor-pointer" onClick={handleSignup}>
+              Sign in
+            </a>
+          </div>
+          {error && <small className="text-red">{error}</small>}
         </form>
       </div>
     </div>
